f7e975430f846a3b98038b1d53f0fef8db280127
charliejuc <charliejuc@gmail.com>
2020-07-14 16:41:22 +0200

    Adds logger prettyed.

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,20 @@ import 'module-alias/register'
 import 'source-map-support/register'
 import { HttpServer } from './HttpServer'
 import { Router } from './Router'
+import pino from 'pino'
 
 const httpServer = new HttpServer(
     Fastify({
-        logger: true
+        logger: pino({
+            prettyPrint: true
+        })
     })
 )
 const router = new Router(httpServer)
 
 router.setupRoutes()
 
-httpServer
-    .listen(3000)
-    .then(console.log)
-    .catch((error) => {
-        console.error(error)
-        process.exit(1)
-    })
+httpServer.listen(3000).catch((error) => {
+    console.error(error)
+    process.exit(1)
+})
