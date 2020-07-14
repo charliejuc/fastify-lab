e117572c2cc0e760a6604185956db04a33f8c35f
charliejuc <charliejuc@gmail.com>
2020-07-14 14:50:59 +0200

    Adds fastify server with sync and async function example.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,26 @@
 import 'source-map-support/register'
 import 'module-alias/register'
-import { hola } from '@/other'
+import Fastify from 'fastify'
 
-console.log('Hello world! :)')
+const fastify = Fastify({
+    logger: true
+})
 
-hola()
+fastify.get('/sync', (request, reply) => {
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    reply.send({ type: 'sync' })
+})
+
+fastify.get('/async', async (request, reply) => {
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    reply.code(201)
+
+    return {
+        type: 'async'
+    }
+})
+
+fastify.listen(3000).catch((error) => {
+    console.error(error)
+    process.exit(1)
+})
